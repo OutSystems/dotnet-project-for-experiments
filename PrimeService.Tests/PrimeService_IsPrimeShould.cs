fbbeb5d74624b1ca067b2724d292db8545874cc4
Andre Mendes <andre.mendes@outsystems.com>
2021-05-21 10:09:00 +0100

    initla commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services {
+    public class PrimeService_IsPrimeShould {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value) {
+
+            var primeService = new PrimeService();
+            var result = primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prime.Services {
+    public class PrimeService {
+        public bool IsPrime(int candidate) {
+            if (candidate < 2) 
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
