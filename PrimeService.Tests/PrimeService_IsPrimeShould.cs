00fcb8da22e5df3395d6ecd84289be8907c50c3f
Andre Mendes <andre.mendes@outsystems.com>
2021-06-08 09:45:38 +0100

    test commit

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -1,13 +1,16 @@
-﻿using Xunit;
-using Prime.Services;
+﻿using Prime.Services;
+using Xunit;
 
-namespace Prime.UnitTests.Services {
-    public class PrimeService_IsPrimeShould {
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
         [Theory]
         [InlineData(-1)]
         [InlineData(0)]
         [InlineData(1)]
-        public void IsPrime_ValuesLessThan2_ReturnFalse(int value) {
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
 
             var primeService = new PrimeService();
             var result = primeService.IsPrime(value);
